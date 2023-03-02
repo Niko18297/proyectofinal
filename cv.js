36a6a577ef979352bdcdf69407d8028bc8b533f4
Niko18297 <126735096+Niko18297@users.noreply.github.com>
2023-03-01 23:02:43 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,22 @@
+
+ /**Los links de los botones deberían redirigir al perfil de personal con los trabajos 
+  * y proyectos pero al ser una persona ficticia puse que te direccione a la página de inicio*/
+
+document.getElementById("boton_linkedin").addEventListener("click", function(){               
+    location.href ='https://ar.linkedin.com/';
+});
+
+
+document.getElementById("boton_computrabajo").addEventListener("click", function(){
+    location.href ='https://ar.computrabajo.com/';
+});
+
+
+document.getElementById("boton_github").addEventListener("click", function(){
+    location.href ='https://github.com/';
+});
+
+
+document.getElementById("boton_domestika").addEventListener("click", function(){
+    location.href ='https://www.domestika.org/es/';
+});
